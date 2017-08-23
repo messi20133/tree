27615fc5c3c5ea947e4f7d8a37db3dfa01fab71e
tangyunlong <tangyunlong@yidian-inc.com>
2017-08-23 20:58:59 +0800

    update:

--- src/main.js
+++ src/main.js
@@ -12,6 +12,13 @@ import Vue from 'vue'
 
 //require('./components/tree.js')(Vue);
 
+const stateMap = {
+  'NOCHECK': 0,
+  'PARTCHECK': 1,
+  'ALLCHECK': 2,
+}
+
+
 Vue.component('CommonNode', {
   template: `<div class="commonnode" @click="checkNode"><span><input type="checkbox" :checked="checked"/><span>{{label}}</span></span><slot></slot></div>`,
   props: ['id', 'label', 'isChecked', 'level' ],
@@ -30,17 +37,29 @@ Vue.component('CommonNode', {
   methods: {
     checkNode () {
       this.checked = !this.checked;
-      this.$parent.hasChecked();
+      this.$parent.hasChecked({
+        id: this.id,
+        checked: this.checked
+      }); 
+    },
+    setCheck (checkedState) {
+      this.checked = checkedState;
     }
   }
 });
 
 Vue.component('ParentNode', {
-  template: `<div class="parentnode"><span @click="checkedAll" @mouseleave="mouseLeave" @mouseenter="mouseEnter"><input type="checkbox" :checked="checked"/><span>{{label}}</span></span><div><slot></slot></div></div>`,
+  template: `<div class="parentnode" @mouseleave="mouseLeave" @mouseenter="mouseEnter"><span @click="checkedAll" ><input type="checkbox" :class="{partSelect: checked == 1}"  :checked="checked == 2"/><span>{{label}}</span></span><div><slot></slot></div></div>`,
   props: ['id', 'label', 'isChecked', 'children', 'level'],
   data: function () {
     return {
-      checked: this.isChecked
+      checked: this.isChecked,
+      childrenList: this.children ? JSON.parse(JSON.stringify(this.children)) : []
+    }
+  },
+  watch: {
+    children (val, old) {
+      this.childrenList = val ? JSON.parse(JSON.stringify(val)) : [];
     }
   },
   computed: {
@@ -49,37 +68,88 @@ Vue.component('ParentNode', {
     }
   },
   methods: {
-    hasChecked () {
-      console.log('sd');
-    },
-    checkedAll () {
-      this.checked = !this.checked;
-      this.setChildrenChecked(this.checked);
-    },
-    setChildrenChecked (flag) {
-      var newChildren =  this.children.map(function(item){
-        item.isChecked = flag; 
-        return $.extend(true, {}, item)
-      });
-      this.children = newChildren;
-    },
-    mouseEnter () {
-      if (this.level == 4) {
-        var root = $(this.$el);
-        var offsetObj = root.offset();
-        root.find('>div').css({
-          position: 'absolute',
-          left: (root.width() + offsetObj.left) + 'px',
-          top: offsetObj.top + 'px',
-        }).show();
+    hasChecked (opt) {
+      var $parent = this.$parent;
+      var index = this.findItemById(opt.id);
+      this.childrenList[index].checked = opt.checked;
+      //获取父节点的状态
+      this.checked = this.getNodeState();
+      if ($parent) {
+        $parent.hasChecked({
+          id: this.id,
+          checked: this.checked,
+          childs: opt
+        });
       }
     },
-    mouseLeave () {
+    checkedAll (checkedState) {
+      if (this.checked == stateMap.ALLCHECK) {
+        this.checked = stateMap.NOCHECK;
+      } else {
+        this.checked = stateMap.ALLCHECK;
+      }
+      this.setChildrenChecked(this.checked);
+      this.$parent.hasChecked({
+        id: this.id,
+        checked: this.checked
+      });
+    },
+    setChildrenChecked (checkedState) {
+      this.childrenList = this.childrenList.map((item) => {
+        item.checked = checkedState;
+        return item;
+      });
+      var $children = this.$children;
+      for (var i=0, l=this.children.length; i<l; i++) {
+        $children[i].setCheck(checkedState);
+      }
+    },
+    setCheck (checkedState) {
+      this.checked = checkedState;
+      this.setChildrenChecked(checkedState);
+    },
+    getNodeState () {
+      var list = this.childrenList;
+      if (list.every((item)=> {
+        if (typeof(item.checked) == 'boolean') {
+          return item.checked;
+        } else {
+          if (item.checked == 2) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      })) {
+        return stateMap.ALLCHECK;
+      } else if (list.some((item) => item.checked)) {
+        return stateMap.PARTCHECK; 
+      } else {
+        return stateMap.NOCHECK;
+      }
+    },
+    findItemById (id) {
+      var index = -1;
+      var list = this.childrenList;
+      for(let i=0, len=list.length; i<len; i++){
+        if (list[i].id == id) {
+          index = i;
+          break;
+        }
+      }
+      return index;
+    },
+    mouseEnter (e) {
       if (this.level == 4) {
         var root = $(this.$el);
-        root.find('>div').css({
-          position: 'static',
-        }).hide();
+        root.find('>div').show();
+      }
+    },
+    mouseLeave (e) {
+      e.stopPropagation();
+      if (this.level == 4) {
+        var root = $(this.$el);
+        root.find('>div').hide();
       }
     }
   }
@@ -90,14 +160,20 @@ var loc = require('./components/loc.js');
 Vue.component("TreeList", {
   render: function (h) {
     var list = this.showList(this.dataList, h, 0);
-    return h('div', {}, list);
+    return h('div', {
+      on: {
+        click: this.clickHandle
+      }
+    }, list);
   },
   created: function () {
     this.$nextTick(() => {
       var $root = $(this.$el);
       $root.find('.level_3>div>.level_4').css('display', 'inline-block');
-      $root.find('.level_3>div>.level_4>div').hide();
+      $root.find('.level_3>div>.level_4>div').addClass('dropdown').hide();
+      $root.find('.level_3>div>.level_4>div>.level_5').css('display', 'inline-block');
     });
+    var that = this;
   },
   methods: {
     showList: function (data, h, level) {
@@ -112,7 +188,8 @@ Vue.component("TreeList", {
               children: item.children,
               isChecked: !!item.isChecked,
               level: level + 1
-            }
+            },
+            key: item.id
           };
           var childrenArray = [];
           
@@ -141,18 +218,78 @@ Vue.component("TreeList", {
         }
         return result;    
       }
+    },
+    hasChecked: function (opt) {
+      this.setDataListChecked(this.dataList, opt);
+    },
+    setDataListChecked: function (list, opt) {
+      var index = this.findItemById(list, opt.id);
+      list[index].checked = opt.checked;
+      if (opt.checked == 1) {
+        if (opt.childs) {
+          this.setDataListChecked(list[index].children, opt.childs);
+        } else {
+          this.collectSelected(opt.id, opt.checked);
+        }
+      } else {
+        this.setAllChecked(list[index], opt.checked == stateMap.ALLCHECK);
+      }
+    },  
+    setAllChecked (list, flag) {
+      if (list.children) {
+        list.checked = flag ? stateMap.ALLCHECK : stateMap.NOCHECK ;
+        var children = list.children;
+        for (let i=0, len=children.length; i<len; i++) {
+          children[i].checked = flag;
+          if (children[i].children) {
+            this.setAllChecked(children[i], flag);
+          } else {
+            this.collectSelected(children[i].id, flag);
+          }
+        }
+      } else {
+        list.checked = flag;
+        this.collectSelected(list.id, flag);
+      }
+    },
+    findItemById (list, id) {
+      var index = -1;
+      for(let i=0, len=list.length; i<len; i++){
+        if (list[i].id == id) {
+          index = i;
+          break;
+        }
+      }
+      return index;
+    },
+    clickHandle: function () {
+      debugger;
+      console.log('sdfsdf');
+    },
+    collectSelected: function (id, flag) {
+      var selectedData = this.selectedData;
+      var index = selectedData.indexOf(id);
+      if (flag) {
+        selectedData.push(id);
+      } else {
+        selectedData.splice(index, 1);
+      }
     }
   },
   data: function () {
     return {
-      dataList: [loc]  
+      dataList: [loc],
+      selectedData: []  
     }
   }
 });
 
-
+require('./app.css');
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<div><TreeList/></div>',
+  methods: {
+    
+  }
 })
